1cd4dea35dc5939edaa5993156e9bc7446fce592
Satssuki <danielarya185@gmail.com>
2021-06-28 02:54:38 +0700

    fix handler

--- forward-message/app.js
+++ forward-message/app.js
@@ -1,6 +1,6 @@
 const { Interact } = require("@satsuki/stalync-sdk");
 const interact = new Interact("6c6ba8b16886", "SimpleChatApp");
 
-module.exports = (clientID, message) => {
+module.exports.handle = (clientID, message) => {
     interact.reply(clientID, message);
 }
